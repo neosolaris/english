497b96a884f2a03c723f55a3ef249315679b5996
neosolaris <batutah@gmail.com>
2026-02-02 04:25:00 +0900

    update files

---
+++ README.md
@@ -0,0 +1,7 @@
+
+00001.txt
+Let me get you another shaker.
+
+00002.txt
+It can cause tooth decay and contributes to obesity.
+
---
+++ update.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+GTTS="gtts"
+OUT="README.md"
+
+# make tts files
+echo "[ MAKE TTS FILES ]"
+$GTTS
+
+echo "[ MAKE LIST FILES ]"
+for file in *.txt
+do
+	echo $file >> $OUT
+	cat $file >> $OUT
+	echo >> $OUT
+done
+
+git add .
+git commit -m "update files" .
+git push
